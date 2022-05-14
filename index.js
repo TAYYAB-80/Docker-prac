ecf7842e6f5a0b5caa98e9b4caba123b3d9e433e
Tayyab-it <tayyabaman80@gmail.com>
2022-05-14 15:08:23 +0500

    added home end point

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app=express()
 app.get('/',(req,res)=>{
     res.json("Hello Inside Docker!")
 })
+app.get('/home',(req,res)=>{
+    res.json("Welcome! To Home")
+})
 const PORT=process.env.PORT||3000
 app.listen(PORT,()=>{
     console.log(`Server is Active At PORT ${PORT}`)
